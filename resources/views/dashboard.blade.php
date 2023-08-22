fffc41166b6e38a4fe14319d528812a15476586a
Shohruh999 <shohruhegamberdiyev9@gmail.com>
2023-08-22 16:41:31 +0500

    prepared a design for the manager

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,84 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+
                     @if (auth()->user()->role->name == 'manager')
-                        maneger
+                        <span class="text-blue-500 font-bold text-xl">Received Applications</span>
+                        <!-- component -->
+                        <!-- component -->
+                        <!-- This is an example component -->
+                        <div class='mt-5'>
+                            <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div
+                                            class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                                        </div>
+                                        <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button
+                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: 5</button>
+                                        <div class="text-xs text-neutral-500">2 hours ago</div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
+                                        neque?</div>
+                                    <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
+                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
+                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
+                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
+                                        Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <div class="flex space-x-4 md:space-x-8 mb-3">
+                                            shohruhegamberdiyev9@gmail.com
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='mt-5'>
+                            <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div
+                                            class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                                        </div>
+                                        <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button
+                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id: 5</button>
+                                        <div class="text-xs text-neutral-500">2 hours ago</div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
+                                        neque?</div>
+                                    <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
+                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
+                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
+                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
+                                        Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <div class="flex space-x-4 md:space-x-8 mb-3">
+                                            shohruhegamberdiyev9@gmail.com
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     @else
                         user
                     @endif
