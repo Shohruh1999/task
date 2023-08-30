d4a8cfaa3885864464ff4c755291589bdb493540
Shohruh999 <shohruhegamberdiyev9@gmail.com>
2023-08-30 11:05:28 +0500

    AnswerController is protected  with Gate1

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -4,20 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class AnswerController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('roleChecker:manager')->only('store','create');
-    }
+ // public function __construct()
+    // {
+    //     $this->middleware('roleChecker:manager')->only('store','create');
+    // }
     public function create(Application $application){
+        if (! Gate::allows('store',auth()->user())){
+            abort(404);
+        }
         if (auth()->user()->cannot('store')){
         abort(403);
     }
         return view('answer.create',['application'=>$application]);
     }
     public function store( Application $application, Request $request){
+        if (! Gate::allows('store',auth()->user())){
+            abort(404);
+        }
         if (auth()->user()->cannot('store')){
             abort(403);
         }
