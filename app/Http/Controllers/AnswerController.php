02879925f23700e20a62b1317c20aee3584aac98
Shohruh999 <shohruhegamberdiyev9@gmail.com>
2023-08-30 10:45:40 +0500

    AnswerController is protected

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('roleChecker:manager')->only('store','create');
+    }
     public function create(Application $application){
+        if (auth()->user()->cannot('store')){
+        abort(403);
+    }
         return view('answer.create',['application'=>$application]);
     }
     public function store( Application $application, Request $request){
+        if (auth()->user()->cannot('store')){
+            abort(403);
+        }
         $request->validate([
             'message' =>'required'
         ]);
@@ -18,7 +28,7 @@ class AnswerController extends Controller
             'body' => $request->message,
         ]);
         return redirect()->route('dashboard');
-        
+
     }
     //
 }
